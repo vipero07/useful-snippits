9d15572f399f7de86d1012d2ad8c4f8d2ad10800
vipero07 <yodaissmart@gmail.com>
2014-05-30 16:32:15 -0400

    Create humanreadable.js
    converts numbers to readable numbers i.e. humanReadable(1234567890) = 1.1G humanReadable(1234567890, 'big') = 1.2B

---
+++ humanreadable.js
@@ -0,0 +1,24 @@
+(function () {
+  //big type is for big numbers i.e. 1234 = 1.2k, 1234567890 = 1.2B
+  //bit is for bits/bytes to kb, mb, gb, etc, defaults to bit
+  var bigPrefix = ' kMBTPEZYXWVU', bigKilo = 1000,
+      bitPrefix = ' KMGTPEZYXWVU', bitKilo = 1024;
+  function humanReadable(number, type){
+    var prefix = bitPrefix, kilo = bitKilo;
+    if(type && type=='big'){
+      prefix = bigPrefix;
+      kilo = bigKilo;
+    }
+    var retValue = 0;
+    if (typeof number == "number") {
+        if (number < bigKilo) {
+            retValue = number.toString();
+        } else {
+            var e = (Math.log(number) / Math.log(kilo)) | 0;
+            retValue = Number((number / Math.pow(kilo, e)).toString().slice(0, 3)) + prefix.charAt(e);
+        }
+    }
+    return retValue;
+  }
+  window.humanReadable = humanReadable;
+})();
