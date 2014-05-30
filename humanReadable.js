ff29725b00b5ac1457a034aefd670022a3399bda
vipero07 <yodaissmart@gmail.com>
2014-05-30 16:48:21 -0400

    Update humanReadable.js

--- humanReadable.js
+++ humanReadable.js
@@ -1,22 +1,19 @@
-(function () {
-  //big type is for big numbers i.e. 1234 = 1.2k, 1234567890 = 1.2B
-  //bit is for bits/bytes to kb, mb, gb, etc, defaults to bit
-  function humanReadable(number, type){
-    var prefix = ' kMGTPEZYXWVU', kilo = 1024;
-    if(type && type=='big'){
-      prefix = prefix.replace('G','B');
-      kilo = 1000;
-    }
-    var retValue = 0;
-    if (typeof number == "number") {
-        if (number < kilo) {
-            retValue = number.toString();
-        } else {
-            var e = (Math.log(number) / Math.log(kilo)) | 0;
-            retValue = Number((number / Math.pow(kilo, e)).toString().slice(0, 3)) + prefix.charAt(e);
-        }
-    }
-    return retValue;
+//big type is for big numbers i.e. 1234 = 1.2k, 1234567890 = 1.2B
+//bit is for bits/bytes to kb, mb, gb, etc, defaults to bit
+function humanReadable(number, type){
+  var prefix = ' kMGTPEZYXWVU', kilo = 1024;
+  if(type && type=='big'){
+    prefix = prefix.replace('G','B');
+    kilo = 1000;
   }
-  window.humanReadable = humanReadable;
-})();
+  var retValue = 0;
+  if (typeof number == "number") {
+      if (number < kilo) {
+          retValue = number.toString();
+      } else {
+          var e = (Math.log(number) / Math.log(kilo)) | 0;
+          retValue = Number((number / Math.pow(kilo, e)).toString().slice(0, 3)) + prefix.charAt(e);
+      }
+  }
+  return retValue;
+}
